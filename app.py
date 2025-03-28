07193040462cb08cd1bfd32c66239e5291dc8ec1
Kellan14 <86698584+Kellan14@users.noreply.github.com>
2025-03-28 15:31:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -111,10 +111,7 @@ if seasons_to_process != st.session_state.previous_seasons:
         st.session_state["seasons_to_process"] = seasons_to_process
         
         # Load and process data
-        all_data = load_all_json_files(repo_dir, seasons_to_process)
-        result_df, debug_outputs, team_player_stats, twc_player_stats = main(
-        all_data, selected_team, selected_venue, st.session_state.roster_data, st.session_state["column_config"]
-    )
+        from mainmodule import main
         
         # Update session state
         st.session_state["result_df"] = result_df
